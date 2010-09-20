204034940bb344061e261ea9688c5627afab1f70
Peter Maydell <peter.maydell@linaro.org>
2010-09-20 19:06:52 +0100

    Basic dump and compare of x86 register info.
    Note that this will declare a mismatch because:
     * we do not clean up the regstate so the two ends match at start of test
     * we do not mask out ESP/EIP/UESP from transferred data
    Also we currently treat all instructions as indicating 'end of test'.

--- risu_i386.c
+++ risu_i386.c
@@ -1,15 +1,38 @@
 /* Copyright 2010 Linaro Limited */
 
 #include <stdio.h>
+#include <ucontext.h>
+#include <string.h>
 
 #include "risu.h"
 
-int send_register_info(int sock, void *uc)
+/* This is the data structure we pass over the socket.
+ * It is a simplified and reduced subset of what can
+ * be obtained with a ucontext_t*
+ */
+struct reginfo
 {
-   return send_data_pkt(sock, "S", 1);
+      gregset_t gregs;
+};
+
+struct reginfo master_ri, apprentice_ri;
+
+static void fill_reginfo(struct reginfo *ri, ucontext_t *uc)
+{
+   int i;
+   for (i = 0; i < NGREG; i++)
+   {
+      ri->gregs[i] = uc->uc_mcontext.gregs[i];
+   }
 }
 
-static unsigned char cmd;
+
+int send_register_info(int sock, void *uc)
+{
+   struct reginfo ri;
+   fill_reginfo(&ri, uc);
+   return send_data_pkt(sock, &ri, sizeof(ri));
+}
 
 /* Read register info from the socket and compare it with that from the
  * ucontext. Return 0 for match, 1 for end-of-test, 2 for mismatch.
@@ -17,11 +40,29 @@ static unsigned char cmd;
  */
 int recv_and_compare_register_info(int sock, void *uc)
 {
-   recv_data_pkt(sock, &cmd, 1);
-   int resp = (cmd == 'S') ? 1 : 2;
+   fill_reginfo(&master_ri, uc);
+   recv_data_pkt(sock, &apprentice_ri, sizeof(apprentice_ri));
+   int resp = 1;
    send_response_byte(sock, resp);
    return resp;
 }
+
+static char *regname[] = 
+{
+   "GS", "FS", "ES" ,"DS", "EDI", "ESI", "EBP", "ESP",
+   "EBX", "EDX", "ECX", "EAX", "TRAPNO", "ERR", "EIP",
+   "CS", "EFL", "UESP", "SS", 0
+};
+
+static void dump_reginfo(struct reginfo *ri)
+{
+   int i;
+   for (i = 0; i < NGREG; i++)
+   {
+      fprintf(stderr, "  %s: %x\n", regname[i] ? regname[i] : "???", ri->gregs[i]);
+   }
+}
+
 
 /* Print a useful report on the status of the last comparison
  * done in recv_and_compare_register_info(). This is called on
@@ -31,6 +72,16 @@ int recv_and_compare_register_info(int sock, void *uc)
  */
 int report_match_status(void)
 {
-   fprintf(stderr, "match status: command %c\n", cmd);
-   return (cmd == 'S') ? 0 : 1;
+   fprintf(stderr, "match status...\n");
+   fprintf(stderr, "master reginfo:\n");
+   dump_reginfo(&master_ri);
+   fprintf(stderr, "apprentice reginfo:\n");
+   dump_reginfo(&apprentice_ri);
+   if (memcmp(&master_ri, &apprentice_ri, sizeof(master_ri)) == 0)
+   {
+      fprintf(stderr, "match!\n");
+      return 0;
+   }
+   fprintf(stderr, "mismatch!\n");
+   return 1;
 }
