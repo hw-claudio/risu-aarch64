c483e5d5d4117bc7a8c00c7cfb2aa32cf968b6b2
Peter Maydell <peter.maydell@linaro.org>
2010-09-22 12:59:06 +0100

    Add handling of ARM general registers.
    This is sufficient for the simple test case to give a match.

--- risu_arm.c
+++ risu_arm.c
@@ -1,8 +1,65 @@
 /* Copyright 2010 Linaro Limited */
 
+#include <stdio.h>
+#include <ucontext.h>
+#include <string.h>
+
+#include "risu.h"
+
+/* This is the data structure we pass over the socket.
+ * It is a simplified and reduced subset of what can
+ * be obtained with a ucontext_t*
+ */
+struct reginfo
+{
+    uint32_t faulting_insn;
+    uint32_t gpreg[16];
+    uint32_t cpsr;
+};
+
+struct reginfo master_ri, apprentice_ri;
+
+void advance_pc(void *vuc)
+{
+   // TODO move forward by 2 if in Thumb
+   ucontext_t *uc = vuc;
+   uc->uc_mcontext.arm_pc += 4;
+}
+
+static int insn_is_eot_marker(uint32_t insn)
+{
+   return (insn == 0xe7fe5af1);
+}
+
+static void fill_reginfo(struct reginfo *ri, ucontext_t *uc)
+{
+   ri->gpreg[0] = uc->uc_mcontext.arm_r0;
+   ri->gpreg[1] = uc->uc_mcontext.arm_r1;
+   ri->gpreg[2] = uc->uc_mcontext.arm_r2;
+   ri->gpreg[3] = uc->uc_mcontext.arm_r3;
+   ri->gpreg[4] = uc->uc_mcontext.arm_r4;
+   ri->gpreg[5] = uc->uc_mcontext.arm_r5;
+   ri->gpreg[6] = uc->uc_mcontext.arm_r6;
+   ri->gpreg[7] = uc->uc_mcontext.arm_r7;
+   ri->gpreg[8] = uc->uc_mcontext.arm_r8;
+   ri->gpreg[9] = uc->uc_mcontext.arm_r9;
+   ri->gpreg[10] = uc->uc_mcontext.arm_r10;
+   ri->gpreg[11] = uc->uc_mcontext.arm_fp;
+   ri->gpreg[12] = uc->uc_mcontext.arm_ip;
+   ri->gpreg[14] = uc->uc_mcontext.arm_lr;
+   ri->gpreg[13] = 0xdeadbeef;
+   ri->gpreg[15] = uc->uc_mcontext.arm_pc - image_start_address;
+   ri->cpsr = uc->uc_mcontext.arm_cpsr;
+
+   // TODO thumb mode?
+   ri->faulting_insn = *((uint32_t*)uc->uc_mcontext.arm_pc);
+}
+
 int send_register_info(int sock, void *uc)
 {
-    return 1;
+   struct reginfo ri;
+   fill_reginfo(&ri, uc);
+   return send_data_pkt(sock, &ri, sizeof(ri));
 }
 
 /* Read register info from the socket and compare it with that from the
@@ -11,7 +68,37 @@ int send_register_info(int sock, void *uc)
  */
 int recv_and_compare_register_info(int sock, void *uc)
 {
-    return 1;
+   int resp;
+   fill_reginfo(&master_ri, uc);
+   recv_data_pkt(sock, &apprentice_ri, sizeof(apprentice_ri));
+   if (memcmp(&master_ri, &apprentice_ri, sizeof(master_ri)) != 0)
+   {
+      /* mismatch */
+      resp = 2;
+   }
+   else if (insn_is_eot_marker(master_ri.faulting_insn))
+   {
+      /* end of test */
+      resp = 1;
+   }
+   else
+   {
+      /* either successful match or expected undef */
+      resp = 0;
+   }
+   send_response_byte(sock, resp);
+   return resp;
+}
+
+static void dump_reginfo(struct reginfo *ri)
+{
+   int i;
+   fprintf(stderr, "  faulting insn %x\n", ri->faulting_insn);
+   for (i = 0; i < 16; i++)
+   {
+      fprintf(stderr, "  r%d: %x\n", i, ri->gpreg[i]);
+   }
+   fprintf(stderr, "  cpsr: %x\n", ri->cpsr);
 }
 
 /* Print a useful report on the status of the last comparison
@@ -22,7 +109,18 @@ int recv_and_compare_register_info(int sock, void *uc)
  */
 int report_match_status(void)
 {
-    return 0;
+   fprintf(stderr, "match status...\n");
+   fprintf(stderr, "master reginfo:\n");
+   dump_reginfo(&master_ri);
+   fprintf(stderr, "apprentice reginfo:\n");
+   dump_reginfo(&apprentice_ri);
+   if (memcmp(&master_ri, &apprentice_ri, sizeof(master_ri)) == 0)
+   {
+      fprintf(stderr, "match!\n");
+      return 0;
+   }
+   fprintf(stderr, "mismatch!\n");
+   return 1;
 }
 
 
